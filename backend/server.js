0a0fa27947554d465e28becec18556c2514e3f19
pritimahesh <pritimahesh52@gmail.com>
2026-08-14 00:41:27 +0530

    fourth commit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 connectDB();
 
 app.use(cors({ origin: [
-    'https://ai-trip-planner-vb7s.vercel.app/', 
+    'https://ai-trip-planner-b5zs.vercel.app', 
     'http://localhost:5173'         
   ],
   credentials: true
