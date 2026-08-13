d69e352b6391081c090e8eab1deca63011eb1ca7
pritimahesh <pritimahesh52@gmail.com>
2026-08-14 00:34:19 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,12 @@ const app = express();
 
 connectDB();
 
-app.use(cors({ origin: process.env.CLIENT_URL || "*" }));
+app.use(cors({ origin: [
+    'https://ai-trip-planner-vb7s.vercel.app/', 
+    'http://localhost:5173'         
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
