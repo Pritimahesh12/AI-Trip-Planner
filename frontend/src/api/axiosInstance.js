1979f3a3bff09a2985f74c389438f2857d1ce88a
pritimahesh <pritimahesh52@gmail.com>
2026-08-15 17:45:13 +0530

    fifth commit

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://ai-trip-planner-backend-hsq2.onrender.com",
+  baseURL: "https://ai-trip-planner-backend-hsq2.onrender.com/api",
 });
 
 axiosInstance.interceptors.request.use((config) => {
